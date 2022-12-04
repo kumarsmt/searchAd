aea899f5948e39fc33a0309f1c1b29210b9ee1b0
sumit <kumarsmt88@gmail.com>
2022-12-05 01:28:36 +0530

    fetch

--- frontend/search/src/search.js
+++ frontend/search/src/search.js
@@ -6,7 +6,7 @@ export const Search = () => {
     // const navigate = useNavigate();
     const handleSubmit = async() => {
 
-        const res = await fetch(`http://localhost:9000/${keyword}`, {
+        const res = await fetch(`https://sum-search.onrender.com/`, {
             headers: {
                 'Content-Type': 'application/json'
             },
